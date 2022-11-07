1623dfe8f76d0872ffb599c8ba5769f23cda70b2
MichaelAI0 <99698667+MichaelAl0@users.noreply.github.com>
2022-11-06 23:09:30 -0600

    ADD: Auth Routers/ Validators

---
+++ packages/server/routers/authRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const Yup = require("yup");
+const validateForm = require("../controllers/validateForm");
+
+
+
+router.post("/login", (req, res) => {
+  validateForm(req,res);
+});
+
+router.post("/signup", (req, res) => {
+  validateForm(req,res);
+});
+
+module.exports = router;
